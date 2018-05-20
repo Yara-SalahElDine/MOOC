f86cfd5c70a6393921c6fb7419cfd913d9d1db60
Yara-SalahElDine <yarasalah94@hotmail.com>
2018-05-20 18:24:21 +0200

    test

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class CourseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "the truth" do
+    course = Course.new(title: 'OS')
+    assert_equal 'OS', course.title
+   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "the truth" do
+     user = User.new(name: 'yara',email: 'yarasalah94@hotmail.com')
+     assert_equal 'yara', user.name
+     assert_equal 'yarasalah94@hotmail.com', user.email
+   end
 end
